97b88d9999d0f4148dbca9d6fd36c39f479b7a86
darshak prajapati <darshak1997@outlook.com>
2022-12-11 12:58:53 +0530

    API Structure Commit

---
+++ OperationAPI/OperationAPI/Controllers/EmployeeInfoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OperationDomian.Entities;
+using OperationRepository.Repositories;
+
+namespace OperationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeInfoController : Controller
+    {
+        EmployeeInfoRepository objemployee = new EmployeeInfoRepository();
+
+        [HttpGet]
+        [Route("Index")]
+        public IEnumerable<EmployeeInfo> Index()
+        {
+            return objemployee.GetEntity();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public int Create(EmployeeInfo employee)
+        {
+            return objemployee.AddEntity(employee);
+        }
+
+        [HttpPut]
+        [Route("Edit")]
+        public int Edit(EmployeeInfo employee)
+        {
+            return objemployee.UpdateEntity(employee);
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objemployee.DeleteEntity(id);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Solution
+This is Simple Structure Code. 
